b19a0fa444eb50c0cd27b2a8a3514476a2f3558b
FreedomCoder <matiasbrutti@gmail.com>
2008-08-28 11:17:34 -0300

    minor changes and test

---
+++ Colorize.rb
@@ -0,0 +1,5 @@
+# load lib files
+require "lib/Colorize.rb"
+
+
+
--- test/test.rb
+++ test/test.rb
@@ -1,11 +1,14 @@
 # Simple tests
+require "Colorize"
+
 class Klass
   include Colorize
   def initialize
-    system("echo #{red("hola")}")
+    system("echo #{red("hola")} This is a #{blue("test")}")
   end
 end
 
 Klass.new
 
-System("echo #{Colorize.white("hola2")}")
+system("echo #{Colorize.white("hola2")} This is a #{Colorize.blue("test")}")
+system("echo #{Colorize.red Colorize.on_white("Hola3")}")
